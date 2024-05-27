c743dbceb6ac363a8bfe0b852c9af5059714ec00
Devansh <devanshchawla116@gmail.com>
2024-05-27 00:37:59 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "logger.h"
+#include<iostream>
+#include<stdio.h>
+#include<thread>
+
+void logTest(int n)
+{
+    Logger::Info("Test %d", n);
+}
+
+int main()
+{
+    std::thread threads[10];
+
+    for(int i = 0; i < 10; i++) {
+        threads[i] = std::thread(logTest, i);
+        threads[i].join();
+    }
+}
